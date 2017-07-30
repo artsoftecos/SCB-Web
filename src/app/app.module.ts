b8ef94a7d58c4153a5017177a5c36afb2fca227d
Daniel Melgarejo <dhmelgarejo@gmail.com>
2017-07-30 16:35:38 -0500

    Interfaz con materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,42 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HttpModule }    from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdCardModule,
+  MdCheckboxModule,
+  MdChipsModule,
+  MdCoreModule,
+  MdDatepickerModule,
+  MdDialogModule,
+  MdExpansionModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdNativeDateModule,
+  MdPaginatorModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdSortModule,
+  MdTableModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,
+  MdDialog,
+  MdDialogRef,
+} from '@angular/material';
 
 //Modulos
 import { ConvocatoryModule } from './convocatory/convocatory.module'
@@ -11,26 +47,77 @@ import { SharedModule } from './shared/shared.module'
 
 //Components
 import { DemoUploadFilesComponent } from './Demos/demo-upload-files/demo-upload-files.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent, ModalUsuario } from './home/home.component';
 
+import {CdkTableModule} from '@angular/cdk';
+
+import 'hammerjs';
 
 @NgModule({
+  exports: [
+    CdkTableModule,
+    MdAutocompleteModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdChipsModule,
+    MdCoreModule,
+    MdDatepickerModule,
+    MdDialogModule,
+    MdExpansionModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdNativeDateModule,
+    MdPaginatorModule,
+    MdProgressBarModule,
+    MdProgressSpinnerModule,
+    MdRadioModule,
+    MdRippleModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdSnackBarModule,
+    MdSortModule,
+    MdTableModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+  ]
+})
+export class MaterialModule {}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    HttpModule,
+    MaterialModule,
+
+    ConvocatoryModule,
+    AppRoutingModule,
+    SharedModule,
+    MdDialogModule,
+  ],
   declarations: [
     AppComponent,
    //se puede eliminar, si se quita el demo de arhivos
     DemoUploadFilesComponent,
-   HomeComponent
+    HomeComponent,
+    ModalUsuario,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-    ConvocatoryModule,
-    AppRoutingModule,
-    HttpModule,
-    SharedModule
+  entryComponents: [
+    ModalUsuario
   ],
   providers: [],
   bootstrap: [AppComponent],
   exports: []
 })
-export class AppModule { }
+export class AppModule {}
+
+// platformBrowserDynamic().bootstrapModule(PlunkerAppModule);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  fullImagePath: string;
 
-  constructor() {         
-    }
+  constructor(public dialog: MdDialog) {       
+    this.fullImagePath = '../../assets/img/img1.jpg'  
+  }
 
   ngOnInit() {
   }
